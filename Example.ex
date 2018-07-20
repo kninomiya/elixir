0c42e376720161d198779f5375286f92d4b8bc52
efg.river <efg.river@gmail.com>
2018-07-20 09:11:47 +0900

    mv Example.ex, Greeting.ex

--- Example.ex
+++
@@ -1,6 +0,0 @@
-defmodule Sayings.Example do
-  alias Sayings.Greetings
-  def greeting(name), do: Greetings.sayHello(name)
-end
-
-
--- Greeting.ex
+++
@@ -1,4 +0,0 @@
-defmodule Sayings.Greetings do
-  def sayHello(name), do:  "Hi, #{name}"
-end
-
