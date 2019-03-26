fdbd25a4140adabaa242d40a9dc53fc97f4de6c9
kninomiya <kounin2017@gmail.com>
2019-03-27 00:53:54 +0900

    third commit

---
+++ 07.ex
@@ -0,0 +1,8 @@
+template = fn (x,y,z) -> "#{x}時の#{y}は#{z}" end
+
+IO.inspect template.(12, "気温", 22.4)
+
+# emplate = fn (x,y,z) -> "#{x}時の#{y}は#{z}" end
+# Function<18.128620087/3 in :erl_eval.expr/5>
+# template.(12, "気温", 22.4)
+# "12時の気温は22.4"
