708dd2cd3ec2e244d41f38cf6693e08ca3490668
kninomiya <kounin2017@gmail.com>
2019-03-30 13:06:12 +0900

    11.ex

--- 10.ex
+++ 10.ex
@@ -4,3 +4,9 @@ File.stream!(filepath)
 |> Enum.to_list
 |> length
 |> IO.inspect
+
+# モジュール名を省略できるモジュールもある。（例:Kernelモジュール）
+# iex(3)> File.stream!(filepath) |> Enum.to_list |> Kernel.length
+# 24
+# iex(4)> File.stream!(filepath) |> Enum.to_list |> length
+# 24
---
+++ 11.ex
@@ -0,0 +1,5 @@
+filepath = "hightemp.txt"
+
+File.read!(filepath)
+|> String.replace("\t"," ")
+|> IO.inspect
