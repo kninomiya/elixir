c4c52d7c667828593a5c1f851d2e3427e147c8ec
kninomiya <kounin2017@gmail.com>
2019-04-17 01:08:43 +0900

    12-1ex

--- 12-1.ex
+++ 12-1.ex
@@ -16,7 +16,7 @@ File.stream!(filepath)
 |> Stream.map(&String.replace(&1, "\t", " "))
 |> Stream.map(&String.split(&1, " "))
 |> Stream.map(&Enum.at(&1,1))
-|> Stream.into( result_file2, &( &1<>"\n" ))
+|> Stream.into( result2_file, &( &1<>"\n" ))
 |> Stream.run()
 
 # 12と13やったらdefmoduleで書いてみる
--- 12.ex
+++ 12.ex
@@ -1,8 +1,10 @@
+filepath = "hightemp.txt"
+
 result_file = File.stream!("./col1.txt", [:write])
 result2_file = File.stream!("./col2.txt", [:write])
 
-File.stream!(filepath)|> Stream.map(&String.replace(&1, "\t", " ")) |> Stream.map(&String.split(&1, " ")) |> Stream.map(&Enum.at(&1,0)) |> Stream.into( result_file, &( &1<>"\n" )) |> Stream.run()
+File.stream!(filepath)|> Stream.map(&String.replace(&1, "\t", " "))|> Stream.map(&String.split(&1, " ")) |> Stream.map(&Enum.at(&1,0)) |> Stream.into( result_file, &( &1<>"\n" )) |> Stream.run()
 
-File.stream!(filepath)|> Stream.map(&String.replace(&1, "\t", " ")) |> Stream.map(&String.split(&1, " ")) |> Stream.map(&Enum.at(&1,1)) |> Stream.into( result_file2, &( &1<>"\n" )) |> Stream.run()
+File.stream!(filepath)|> Stream.map(&String.replace(&1, "\t", " ")) |> Stream.map(&String.split(&1, " ")) |> Stream.map(&Enum.at(&1,1)) |> Stream.into( result2_file, &( &1<>"\n" )) |> Stream.run()
 
 # 12と13やったらdefmoduleで書いてみる
